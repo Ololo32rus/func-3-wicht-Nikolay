059ef25cdfacbb6d3137c294fd3cead00c3046cf
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2023-01-15 11:00:27 +0200

    Добавьте файлы проекта.

---
+++ func 3 wicht Nikolay/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дан массив. Вывести на экран сначала его неотрицательные элементы, затем отрицательные.
+/*
+int[] num = { 1, 2, -3, 4, 5, -8, 90, 23, -45, -67 };
+
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] > 0)
+    {
+        Console.WriteLine(num[i]);
+    }
+}
+
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] < 0)
+    {
+        Console.WriteLine(num[i]);
+    }
+}
+*/
+
+
+static void Chet()
+{
+    int[] num = { 1, 2, -3, 4, 5, -8, 90, 23, -45, -67 };
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0)
+        {
+            Console.WriteLine(num[i]);
+        }
+    }
+}
+    Chet();
